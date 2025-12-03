3662b00c105d018e5f4b9a2e4bbfba9d9da0cc20
Amine Benmoussa <121407324+Amine-Nova@users.noreply.github.com>
2025-12-03 12:37:39 +0100

    set full day 100% the hardest ex05 hh

--- Django1/ex07/periodic_table.py
+++ Django1/ex07/periodic_table.py
@@ -1,41 +1,50 @@
-import sys
-
 def cssopen():
-    fcss = open("styles.css", 'w')
-    css = """.main {
-  display: flex;
-  flex-direction: column;
-  width: 1735px;
-}
-.main2 {
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-}
-h4 {
-  margin: 0;
-  text-align: center;
-}
-.wrapper {
-  border: 1px solid black;
-  width: 95px;
-  display: flex;
-  flex-direction: column;
-  margin: 0;
-}
-.p1 {
-  font-size: 15px;
-  align-items: flex-start;
-  margin: 2px;
-}
-.p2 {
-  text-align: center;
-  font-size: 30px;
-  margin: 10px;
-}"""
+    fcss = open("periodic_table.css", 'w')
+    css = """   .main {
+        display: flex;
+        width: 2160px;
+        flex-direction: row;
+      }
+      .main2 {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+      }
+      h4 {
+        margin: 0;
+        text-align: center;
+      }
+      .wrapper {
+        border: 1px solid black;
+        width: 120px;
+        display: flex;
+        flex-direction: column;
+        margin: 0;
+      }
+      .wrapper2 {
+        border: 1px solid rgba(0, 0, 0, 0);
+        width: 120px;
+        display: flex;
+        flex-direction: column;
+        margin: 0;
+      }
+      .p1 {
+        font-size: 15px;
+        align-items: flex-start;
+        margin: 2px;
+      }
+      .molar {
+        text-align: center;
+        margin: 0;
+      }
+      .p2 {
+        text-align: center;
+        font-size: 30px;
+        margin: 10px;
+      }"""
     fcss.write(css)
 
 def get_item(data, delimiter, num):
@@ -43,8 +52,8 @@ def get_item(data, delimiter, num):
     return var[num]
 
 def file():
-    number, name, sign = [], [], []
-    f = open("amine.html", 'w')
+    number, name, sign, molar = [], [], [], []
+    f = open("periodic_table.html", 'w')
     cssopen()
     f2 = open("periodic_table.txt")
     for line in f2:
@@ -53,87 +62,53 @@ def file():
         name.append(get_item(part[0], "=", 0))
         number.append(get_item(part[1], ":", 1))
         sign.append(get_item(part[2], ":", 1))
+        molar.append(get_item(part[3], ":", 1))
 
-    print(len(sign))
+    line1 = [number[0]] + [""] * 16 + [number[1]]
+    line2 = number[2:4] + [""] * 10 + number[4:10] 
+    line3 = number[10:12] + [""] * 10 + number[12:18] 
+    line4 = number[18:36]
+    line5 = number[36:54]
+    line6 = number[54:56] + [""] + number[56:71]
+    line7 = number[71:73] + [""] + number[73:88]
+    lines = [line1, line2, line3, line4, line5, line6, line7]
+    red_numbers = {1,2,3,4,11,12,19,20,37,38,55,56,87,88}
+    blue_ranges = [(21,30), (39,48), (71,80), (103,112)]
     html = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Periodic Table</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-"""
-    html += """ <div class="main">
-    <div style="display: flex; justify-content: space-between; width: 100%">""" 
-    i = 0
-    while i < 2:
-        html += f"""  <div class="wrapper">
-            <p class="p1">{number[i]}</p>
-            <p class="p2">{sign[i]}</p>
-            <h4>{name[i]}</h4>
-        </div>""" 
-        i += 1
-    html += """</div>
-    <div class="main2">
-    <div style="display: flex">"""
-    while i < 4:
-        html += f"""  <div class="wrapper">
-            <p class="p1">{number[i]}</p>
-            <p class="p2">{sign[i]}</p>
-            <h4>{name[i]}</h4>
-        </div>""" 
-        i += 1
-    html += """</div>
-    <div style="display: flex">"""
-    while i < 10:
-        html += f"""  <div class="wrapper">
-            <p class="p1">{number[i]}</p>
-            <p class="p2">{sign[i]}</p>
-            <h4>{name[i]}</h4>
-        </div>\n""" 
-        i += 1
-    html += """</div>
-    </div>
-    <div class="main2">
-    <div style="display: flex">"""
-    while i < 12:
-        html += f"""  <div class="wrapper">
-            <p class="p1">{number[i]}</p>
-            <p class="p2">{sign[i]}</p>
-            <h4>{name[i]}</h4>
-        </div>""" 
-        i += 1
-    html += """</div>
-    <div style="display: flex">"""
-    while i < 18:
-        html += f"""  <div class="wrapper">
-            <p class="p1">{number[i]}</p>
-            <p class="p2">{sign[i]}</p>
-            <h4>{name[i]}</h4>
-        </div>\n""" 
-        i += 1
-    html += """</div>
-    </div>
-    <div style="display: flex">"""
-    while i < 36:
-        html += f"""  <div class="wrapper">
-            <p class="p1">{number[i]}</p>
-            <p class="p2">{sign[i]}</p>
-            <h4>{name[i]}</h4>
-        </div>\n""" 
-        i += 1
-    html += """</div>
-    <div style="display: flex">"""
-    while i < 54:
-        html += f"""  <div class="wrapper">
-            <p class="p1">{number[i]}</p>
-            <p class="p2">{sign[i]}</p>
-            <h4>{name[i]}</h4>
-        </div>\n""" 
-        i += 1
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Periodic Table</title>
+     <link rel="stylesheet" href="periodic_table.css">
+ </head>
+ <body>
+ """
+    for l in lines:
+        html += """\t<div class="main">\n"""
+        for i in l:
+            if int(number[0]) in red_numbers:
+                color = "#f57171"
+            elif any(start <= int(number[0]) <= end for start, end in blue_ranges):
+                color = "#7199f5"
+            else:
+                color = "#f5f571"
+            if i == "":
+                html += """\t<div class="wrapper2"></div>\n"""
+            else:
+                html += f"""\t<div class="wrapper" style="background-color: {color};">
+          <p class="p1">{number[0]}</p>
+          <p class="p2">{sign[0]}</p>
+          <h4>{name[0]}</h4>
+          <p class="molar">{molar[0]}</p>
+        </div>\n"""
+                del number[0]
+                del sign[0]
+                del name[0]
+                del molar[0]
 
+        html += """\t</div>\n"""
+    html+= """</body>\n </html>"""
     f.write(html)
 
 if __name__ == "__main__":

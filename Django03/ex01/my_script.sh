5091c173731c583201d6316deec1106a9fc71a9f
Amine-Nova <bmamine6@gmail.com>
2026-02-14 11:14:02 +0100

    finish dj3 and working on dj4

--- Django03/ex01/my_script.sh
+++ Django03/ex01/my_script.sh
@@ -6,5 +6,5 @@ if [ -d local_lib/path ]; then
     echo "File Already Exists!"
 else
     rm -rf local_lib
-    git clone --progress https://github.com/jaraco/path.git local_lib 2>&1
+    git clone --progress https://github.com/jaraco/path.git local_lib &> path.log
 fi
